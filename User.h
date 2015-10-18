2e040f7e840342ac32231e6a95dcd0f4fa3a01a2
Joel <joel.sprinkle@carleton.ca>
2015-10-18 11:09:27 -0400

    minor edit

--- User.h
+++ User.h
@@ -1,4 +1,42 @@
 #ifndef USER_H
 #define USER_H
 
+#include <string.h>
+
+class User
+{
+protected:
+    int IDNum;          //students start with "1", Admins start with "A"
+    string firstName;
+    string lastName;
+
+public:
+    User(); // default constructor
+    ~User(); // destructor
+};
+
+
+
+class Student : public User
+{
+private:
+    //list of attribues
+    //list of projects
+
+public:
+    Student();
+    ~Student();
+};
+
+
+
+class Admin : public User
+{
+private:
+    //list of projects
+
+public:
+    Admin();
+    ~Admin();
+};
 #endif // USER_H
