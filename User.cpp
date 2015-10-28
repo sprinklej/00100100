a1f3e3d25d908febc2655a65cc6bbdfb14e0558d
Ian <ismith1024@gmail.com>
2015-10-28 19:41:34 -0400

    Student class has qualifications along with gettters and setters

--- User.cpp
+++ User.cpp
@@ -18,11 +18,108 @@ QString User::toString(){
 }
 
 
-Student::Student(QString fn, QString ln, QString id, int a1){
+Student::Student(QString fn, QString ln, QString id, int a1 ){
     firstName = fn;
     lastName = ln;
     IDNum = id;
-    att1=a1;
+
+    att_leader = false;
+    att_avail = QString("FFFFFFFFFFFFFFFFFFFFF");
+    att_2404 = 0;
+    att_3005 = 0;
+    att_coding = 0;
+    att_dbase = 0;
+    att_selfDir = 0;
+    att_writing = 0;
+    att_UI = 0;
+    att_algorithm = 0;
+    att_present = 0;
+    att_teamwork = 0;
+    att_experience = 0;
+    att_testing = 0;
+    att_UML = 0;
+    att_req = 0;
+    att_reliable = 0;
+    att_comm = 0;
+    att_respect = 0;
+    att_creative = 0;
+    att_critic = 0;
+    req_leader = 0;
+    req_2404 = 0;
+    req_3005 = 0;
+    req_coding = 0;
+    req_dbase = 0;
+    req_selfDir = 0;
+    req_writing = 0;
+    req_UI = 0;
+    req_algorithm = 0;
+    req_present = 0;
+    req_teamwork = 0;
+    req_experience = 0;
+    req_testing = 0;
+    req_UML = 0;
+    req_req = 0;
+    req_reliable = 0;
+    req_comm = 0;
+    req_respect = 0;
+    req_creative = 0;
+    req_critic = 0;
+
+}
+
+Student::Student(QString fn, QString ln, QString id, bool cld, QString cav, int c2404,
+                 int c3005, int ccod, int cdb, int csd, int cwr, int cui, int cal, int cpr,
+                 int ctw, int cxp, int ctst, int cuml, int creq, int crel, int ccom, int cres,
+                 int ccre, int ccri, bool crld, int cr2404, int cr3005, int crcod, int crdb,
+                 int crsd, int crwr, int crui, int cral, int crpr, int crtw, int crxp,
+                 int crtst, int cruml, int crreq, int crrel, int crcom, int crres,
+                 int crcre, int crcri){
+    firstName = fn;
+    lastName = ln;
+    IDNum = id;
+
+    att_leader = cld; // : int        -- want to be a leader? yes/no
+    att_avail = cav; // : text        -- represent availability slots TTFTTTTFTTFT (etc.)
+    att_2404 = c2404; // : int        -- grade in 2404
+    att_3005 = c3005; //: int        -- grade in 3005
+    att_coding = ccod; //: int        -- self-assessed coding skill (can be made objective)
+    att_dbase = cdb; //: int        -- self-assessed database design skill (can be made objective)
+    att_selfDir = csd; //: int        -- self-assessed self-directedness
+    att_writing = cwr; //: int     -- self-assessed technical writing skill and grammar (can be made objective)
+    att_UI = cui; //: int        -- self-assessed UI design (can be made objective)
+    att_algorithm = cal; //: int    -- self-assessed algorithms design skill (can be made objective)
+    att_present = cpr; //: int    -- self-assessed verbal presentation skill (can be made objective)
+    att_teamwork = ctw; //: int    -- self-assessed teamwork skill
+    att_experience = cxp; //: int    -- objective experience
+    att_testing = ctst; //: int        -- self-assessed software testing skill
+    att_UML = cuml; //: int        -- self-assessed UML design skill
+    att_req = creq; //: int        -- self-assessed requirements analysis skill
+    att_reliable = crel; //: int    -- self-assessed reliability trait
+    att_comm = ccom; //: int        -- self-assessed communication skill
+    att_respect = cres; //: int    -- self-assessed respectfulness trait
+    att_creative = ccre; //: int    -- self-assessed creativity trait
+    att_critic = ccri; // : int        -- self-assessed openness to constructive criticism
+    req_leader = crld;
+    req_2404 = cr2404;
+    req_3005 = cr3005;
+    req_coding = crcod;
+    req_dbase = crdb;
+    req_selfDir = crsd;
+    req_writing = crwr;
+    req_UI = crui;
+    req_algorithm = cral;
+    req_present = crpr;
+    req_teamwork = crtw;
+    req_experience = crxp;
+    req_testing = crtst;
+    req_UML = cruml;
+    req_req = crreq;
+    req_reliable = crrel;
+    req_comm = crcom;
+    req_respect = crres;
+    req_creative = crcre;
+    req_critic = crcri;
+
 }
 
 Student::~Student(){}
@@ -32,7 +129,93 @@ QString Student::getIDNum(){return IDNum;}          //students start with "1", A
 QString Student::getUserName(){return userName;}
 QString Student::getFirstName(){return firstName;}
 QString Student::getLastName(){return lastName;}
-int Student::getAtt1(){return att1;}
+//int Student::getAtt1(){return att1;}
+
+bool Student::getAtt_leader() {return att_leader  ;}
+QString Student::getAtt_avail() {return att_avail  ;}
+int Student::getAtt_2404() {return att_2404  ;}
+int Student::getAtt_3005() {return att_3005  ;}
+int Student::getAtt_coding() {return att_coding  ;}
+int Student::getAtt_dbase() {return att_dbase  ;}
+int Student::getAtt_selfDir() {return att_selfDir  ;}
+int Student::getAtt_writing() {return att_writing  ;}
+int Student::getAtt_UI() {return att_UI  ;}
+int Student::getAtt_algorithm() {return att_algorithm  ;}
+int Student::getAtt_present() {return att_present  ;}
+int Student::getAtt_teamwork() {return att_teamwork  ;}
+int Student::getAtt_experience() {return att_experience  ;}
+int Student::getAtt_testing() {return att_testing  ;}
+int Student::getAtt_UML() {return att_UML  ;}
+int Student::getAtt_req() {return att_req  ;}
+int Student::getAtt_reliable() {return att_reliable  ;}
+int Student::getAtt_comm() {return att_comm  ;}
+int Student::getAtt_respect() {return att_respect  ;}
+int Student::getAtt_creative() {return att_creative  ;}
+int Student::getAtt_critic() {return  att_critic ;}
+bool Student::getReq_leader() {return req_leader  ;}
+int Student::getReq_2404() {return req_2404  ;}
+int Student::getReq_3005() {return req_3005  ;}
+int Student::getReq_coding() {return req_coding  ;}
+int Student::getReq_dbase() {return req_dbase  ;}
+int Student::getReq_selfDir() {return req_selfDir  ;}
+int Student::getReq_writing() {return req_writing  ;}
+int Student::getReq_UI() {return req_UI  ;}
+int Student::getReq_algorithm() {return  req_algorithm ;}
+int Student::getReq_present() {return req_present  ;}
+int Student::getReq_teamwork() {return req_teamwork  ;}
+int Student::getReq_experience() {return req_experience  ;}
+int Student::getReq_testing() {return req_testing  ;}
+int Student::getReq_UML() {return req_UML  ;}
+int Student::getReq_req() {return req_req  ;}
+int Student::getReq_reliable() {return req_reliable  ;}
+int Student::getReq_comm() {return req_comm  ;}
+int Student::getReq_respect() {return req_respect  ;}
+int Student::getReq_creative() {return  req_creative ;}
+int Student::getReq_critic() {return req_critic  ;}
+
+/////////// SETTERS ////////
+
+void Student::setAtt_leader(bool a){att_leader = a;}
+void Student::setAtt_avail(QString a){att_avail = a;}
+void Student::setAtt_2404(int a){att_2404 = a;}
+void Student::setAtt_3005(int a){att_3005 = a;}
+void Student::setAtt_coding(int a){ att_coding = a;}
+void Student::setAtt_dbase(int a){ att_dbase = a;}
+void Student::setAtt_selfDir(int a){ att_selfDir = a;}
+void Student::setAtt_writing(int a){ att_writing = a;}
+void Student::setAtt_UI(int a){ att_UI = a;}
+void Student::setAtt_algorithm(int a){ att_algorithm = a;}
+void Student::setAtt_present(int a){ att_present = a;}
+void Student::setAtt_teamwork(int a){ att_teamwork = a;}
+void Student::setAtt_experience(int a){att_experience = a;}
+void Student::setAtt_testing(int a){ att_testing = a;}
+void Student::setAtt_UML(int a){ att_UML = a;}
+void Student::setAtt_req(int a){ att_req = a;}
+void Student::setAtt_reliable(int a){att_reliable = a;}
+void Student::setAtt_comm(int a){ att_comm = a;}
+void Student::setAtt_respect(int a){att_respect = a;}
+void Student::setAtt_creative(int a){ att_creative = a;}
+void Student::setAtt_critic(int a){att_critic = a;}
+void Student::setReq_leader(bool){ req_leader = a;}
+void Student::setReq_2404(int a){ req_2404 = a;}
+void Student::setReq_3005(int a){req_3005 = a;}
+void Student::setReq_coding(int a){req_coding = a;}
+void Student::setReq_dbase(int a){req_dbase = a;}
+void Student::setReq_selfDir(int a){ req_selfDi = a;}
+void Student::setReq_writing(int a){ req_writing = a;}
+void Student::setReq_UI(int a){req_UI = a;}
+void Student::setReq_algorithm(int a){ req_algorithm = a;}
+void Student::setReq_present(int a){req_present = a;}
+void Student::setReq_teamwork(int a){ req_teamwork = a;}
+void Student::setReq_experience(int a){req_experience = a;}
+void Student::setReq_testing(int a){ req_testing = a;}
+void Student::setReq_UML(int a){ req_UML = a;}
+void Student::setReq_req(int a){ req_req = a;}
+void Student::setReq_reliable(int a){ req_reliable = a;}
+void Student::setReq_comm(int a){ req_comm = a;}
+void Student::setReq_respect(int a){ req_respect = a;}
+void Student::setReq_creative(int a){req_creative = a;}
+void Student::setReq_critic(int a){req_critic = a;}
 
 
 Admin::Admin(QString fn, QString ln, QString id, int a1){
--- User.h
+++ User.h
@@ -8,10 +8,12 @@ using namespace std;
 class User
 {
 protected:
-    QString IDNum;          //students start with "1", Admins start with "A"
+    QString IDNum;          //students start with "1", Admins start with "A-Z or a-z"
     QString userName;
     QString firstName;
     QString lastName;
+
+
 
 public:
     // constructor/destructor
@@ -30,24 +32,151 @@ public:
 class Student : public User
 {
 protected:
-    int att1;
-    //list of attribues
-    //list of projects
+    //Qualifications//
+    bool att_leader; // : int        -- want to be a leader? yes/no
+    QString att_avail; // : text        -- represent availability slots TTFTTTTFTTFT (etc.)
+    int att_2404; // : int        -- grade in 2404
+    int att_3005; //: int        -- grade in 3005
+    int att_coding; //: int        -- self-assessed coding skill (can be made objective)
+    int att_dbase; //: int        -- self-assessed database design skill (can be made objective)
+    int att_selfDir; //: int        -- self-assessed self-directedness
+    int att_writing; //: int     -- self-assessed technical writing skill and grammar (can be made objective)
+    int att_UI; //: int        -- self-assessed UI design (can be made objective)
+    int att_algorithm; //: int    -- self-assessed algorithms design skill (can be made objective)
+    int att_present; //: int    -- self-assessed verbal presentation skill (can be made objective)
+    int att_teamwork; //: int    -- self-assessed teamwork skill
+    int att_experience; //: int    -- objective experience
+    int att_testing; //: int        -- self-assessed software testing skill
+    int att_UML; //: int        -- self-assessed UML design skill
+    int att_req; //: int        -- self-assessed requirements analysis skill
+    int att_reliable; //: int    -- self-assessed reliability trait
+    int att_comm; //: int        -- self-assessed communication skill
+    int att_respect; //: int    -- self-assessed respectfulness trait
+    int att_creative; //: int    -- self-assessed creativity trait
+    int att_critic; // : int        -- self-assessed openness to constructive criticism
+    bool req_leader;
+    int req_2404;
+    int req_3005;
+    int req_coding;
+    int req_dbase;
+    int req_selfDir;
+    int req_writing;
+    int req_UI;
+    int req_algorithm;
+    int req_present;
+    int req_teamwork;
+    int req_experience;
+    int req_testing;
+    int req_UML;
+    int req_req;
+    int req_reliable;
+    int req_comm;
+    int req_respect;
+    int req_creative;
+    int req_critic;
 
 public:
     // constructor/destructor
     Student(QString, QString, QString, int);
+    Student(QString, QString, QString, bool, QString, int,
+                     int, int, int,  int, int, int, int, int,
+                     int, int, int,  int, int, int, int, int,
+                     int, int, bool, int, int, int, int,
+                     int, int, int,  int, int, int, int,
+                     int, int, int,  int, int, int,
+                     int, int){
     ~Student();
 
-    //QString toString();
-    // setters
 
     // getters
     QString getIDNum();          //students start with "1", Admins start with "A"
     QString getUserName();
     QString getFirstName();
     QString getLastName();
-    int getAtt1();
+    bool getAtt_leader(); // : int        -- want to be a leader? yes/no
+    QString getAtt_avail(); // : text        -- represent availability slots TTFTTTTFTTFT (etc.)
+    int getAtt_2404(); // : int        -- grade in 2404
+    int getAtt_3005(); //: int        -- grade in 3005
+    int getAtt_coding(); //: int        -- self-assessed coding skill (can be made objective)
+    int getAtt_dbase(); //: int        -- self-assessed database design skill (can be made objective)
+    int getAtt_selfDir(); //: int        -- self-assessed self-directedness
+    int getAtt_writing(); //: int     -- self-assessed technical writing skill and grammar (can be made objective)
+    int getAtt_UI(); //: int        -- self-assessed UI design (can be made objective)
+    int getAtt_algorithm(); //: int    -- self-assessed algorithms design skill (can be made objective)
+    int getAtt_present(); //: int    -- self-assessed verbal presentation skill (can be made objective)
+    int getAtt_teamwork(); //: int    -- self-assessed teamwork skill
+    int getAtt_experience(); //: int    -- objective experience
+    int getAtt_testing(); //: int        -- self-assessed software testing skill
+    int getAtt_UML(); //: int        -- self-assessed UML design skill
+    int getAtt_req(); //: int        -- self-assessed requirements analysis skill
+    int getAtt_reliable(); //: int    -- self-assessed reliability trait
+    int getAtt_comm(); //: int        -- self-assessed communication skill
+    int getAtt_respect(); //: int    -- self-assessed respectfulness trait
+    int getAtt_creative(); //: int    -- self-assessed creativity trait
+    int getAtt_critic(); // : int        -- self-assessed openness to constructive criticism
+    bool getReq_leader();
+    int getReq_2404();
+    int getReq_3005();
+    int getReq_coding();
+    int getReq_dbase();
+    int getReq_selfDir();
+    int getReq_writing();
+    int getReq_UI();
+    int getReq_algorithm();
+    int getReq_present();
+    int getReq_teamwork();
+    int getReq_experience();
+    int getReq_testing();
+    int getReq_UML();
+    int getReq_req();
+    int getReq_reliable();
+    int getReq_comm();
+    int getReq_respect();
+    int getReq_creative();
+    int getReq_critic();
+
+    void setAtt_leader(bool);
+    void setAtt_avail(QString);
+    void setAtt_2404(int);
+    void setAtt_3005(int);
+    void setAtt_coding(int);
+    void setAtt_dbase(int);
+    void setAtt_selfDir(int);
+    void setAtt_writing(int);
+    void setAtt_UI(int);
+    void setAtt_algorithm(int);
+    void setAtt_present(int);
+    void setAtt_teamwork(int);
+    void setAtt_experience(int);
+    void setAtt_testing(int);
+    void setAtt_UML(int);
+    void setAtt_req(int);
+    void setAtt_reliable(int);
+    void setAtt_comm(int);
+    void setAtt_respect(int);
+    void setAtt_creative(int);
+    void setAtt_critic(int);
+    void setReq_leader(bool);
+    void setReq_2404(int);
+    void setReq_3005(int);
+    void setReq_coding(int);
+    void setReq_dbase(int);
+    void setReq_selfDir(int);
+    void setReq_writing(int);
+    void setReq_UI(int);
+    void setReq_algorithm(int);
+    void setReq_present(int);
+    void setReq_teamwork(int);
+    void setReq_experience(int);
+    void setReq_testing(int);
+    void setReq_UML(int);
+    void setReq_req(int);
+    void setReq_reliable(int);
+    void setReq_comm(int);
+    void setReq_respect(int);
+    void setReq_creative(int);
+    void setReq_critic(int);
+
 };
 
 
